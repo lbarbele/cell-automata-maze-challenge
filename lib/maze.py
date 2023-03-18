756212ef0af3cefd9111fe0697fc240c41a1231e
lbarbele <luan.arbeletche@gmail.com>
2023-03-18 15:47:42 -0300

    document Maze class and annotate the signature of all methods

--- lib/maze.py
+++ lib/maze.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import random
 
 import matplotlib.pyplot as plt
@@ -5,13 +6,27 @@ import numpy as np
 
 from .cell import Cell
 
+Position = tuple[int, int]
+
 class Maze():
   """
   The Maze class represents a rectangular maze of cell automata and provides methods
-  to analyze its dynamics.
+  to analyze its dynamics. The maze is constructed in its initial state by a numpy
+  matrix of cell values (compatible with the Cell enum). Then, its state can be
+  evolved by successive calls to ```Maze.evolve()```.
+
+  To find the shortest path between some initial position and the finishing point,
+  two methods are provided:
+  - ```Maze.solve_random()```: will try to find a solution by randomly walking through the maze
+  - ```Maze.solve()```: will consider every possible path until some of them leads to the end
   """
 
   def __init__(self, matrix: np.ndarray):
+    """
+    Maze constructor: takes a cell matrix as input, whose values will be checked. The
+    matrix is required to contain exactly one starting and one finishing cell. It is
+    also required that all cell values are valid (i.e. can be converted to the enum Cell).
+    """
     # check input
     if not isinstance(matrix, np.ndarray):
       raise RuntimeError('input matrix is invalid (expected a numpy array)')
@@ -45,10 +60,27 @@ class Maze():
     if self.start is None or self.finish is None:
       raise RuntimeError('input matrix is incomplete: missing start or finish tiles')
 
-  def clone(self):
+  def clone(self) -> Maze:
+    """
+    Returns a deep clone of this maze.
+
+    Arguments: none
+    
+    Returns: a ```Maze``` object, clone if this maze
+    """
     return Maze(self.maze)
   
-  def count_green_neighbours(self, position: tuple[int, int]):
+  def count_green_neighbours(self, position: Position) -> int:
+    """
+    Counts the number of green cells among the direct neighbours of the cell at the
+    given position.
+
+    Arguments:
+    - ```position: tuple[int, int]```: position of the cell around which the number of green cells will
+    be computed
+
+    Returns: an integer corresponding to the count of green neighbours.
+    """
     count = 0
 
     for ij in self.get_neighbours(position):
@@ -57,16 +89,38 @@ class Maze():
     
     return count
 
-  def draw(self, position: tuple[int, int] = None, ms = 20):
-    plt.matshow(self.maze, cmap = 'tab10')
+  def draw(self, position: Position = None, ms: int = 20) -> None:
+    """
+    Draw the maze using ```matplotlib```'s ```matshow``` method. If a position is given
+    as argument, a cursor is drawn at such position and all possible valid moves are
+    marked in the grid.
+
+    Arguments:
+    - ```position: tuple[int, int]```: cursor position
+    - ```ms: int```: cursor size
+
+    Returns: a ```matplotlib.image.AxesImage``` object (returned from ```plt.matshow```)
+    """
+    axes_image = plt.matshow(self.maze, cmap = 'tab10')
     if not (position is None):
       plt.plot(position[1], position[0], 'kD', ms = ms)
       moves = np.array(self.get_valid_moves(position))
       if len(moves) > 0:
         plt.plot(moves[:, 1], moves[:, 0], 'k+')
-    plt.show()
+
+    return axes_image
   
-  def evolve(self, overwrite: bool = True):
+  def evolve(self, overwrite: bool = True) -> Maze:
+    """
+    Apply cell dynamics to every cell in the maze. By default, this maze is overwritten
+    and returned by the method. If the parameter ```overwrite``` is disabled, the maze
+    is not modified and the returned value is a new Maze after the cell evolution.
+
+    Arguments:
+    - ```overwrite: bool```: enable/disable overwrite of this Maze
+
+    Returns: ```self``` if overwrite is enabled or a new ```Maze``` otherwise.
+    """
     other = np.full(self.maze.shape, Cell.WHITE, dtype = int)
 
     for pos in np.ndindex(self.shape):
@@ -74,13 +128,30 @@ class Maze():
 
     if overwrite:
       self.maze = other
+      return self
+    else:
+      return Maze(other)
 
-    return other
+  def get_cell_indices(self, cell_type: Cell) -> list[Position]:
+    """
+    Find all positions of cells of given type.
 
-  def get_cell_indices(self, cell_type: Cell):
+    Arguments:
+    - ```cell_type: Cell```: type of cell to be found
+
+    Returns: a list of ```Position``` containing the positions of the given cell type.
+    """
     return [pos for pos, c in np.ndenumerate(self.maze) if c == cell_type]
   
-  def get_mutation(self, position: tuple[int, int]):
+  def get_mutation(self, position: Position) -> Cell:
+    """
+    Apply cell dynamics to determine how the cell at the given position will evolve.
+
+    Arguments:
+    - ```position: Position```: position of the cell of interest
+
+    Returns: a ```Cell``` corresponding to the cell evolution
+    """
     type = self.maze[position]
 
     if type == Cell.START or type == Cell.FINISH:
@@ -94,7 +165,18 @@ class Maze():
     else:
       return Cell.WHITE
 
-  def get_neighbours(self, position: tuple[int, int], include_diag: bool = True):
+  def get_neighbours(self, position: Position, include_diag: bool = True) -> list[Position]:
+    """
+    Determines all neighbour positions of a given cell, taking into account border. By default,
+    searches for neighbours in all (up to) 8 neighbouring cells. By setting ```include_diag```
+    to false, only horizontal and vertical neighbours are returned.
+
+    Arguments:
+    - ```position: Position```: position from where to compute neighbours
+    - ```include_diag: bool```: include diagonal neighbours
+
+    Returns: a list of ```Position``` objects corresponding to the cell neighbours
+    """
     i, j = position
 
     if i < 0: i = self.rows + i
@@ -109,7 +191,16 @@ class Maze():
 
     return [(k, l) for k, l in nb if (k>=0 and l>=0 and k<self.rows and l<self.cols)]
 
-  def get_valid_moves(self, position: tuple[int, int] = None):
+  def get_valid_moves(self, position: Position = None) -> list[Position]:
+    """
+    Get a list of valid moves for a cursor at the given position. That is, computes all
+    movements, starting at position, that will end up in a white cell.
+
+    Arguments:
+    - ```position: Position```: cursor position
+
+    Returns: a list of ```Position``` objects representing valid moves
+    """
     if position is None:
       position = self.start
 
@@ -120,7 +211,19 @@ class Maze():
     valid_moves = [ij for ij in nb if self.get_mutation(ij) != Cell.GREEN]
     return valid_moves
   
-  def random_walk(self, position: tuple[int, int], evolve: bool = False):
+  def random_walk(self, position: Position, evolve: bool = False) -> Position | None:
+    """
+    Takes a cursor at the given position, computes all valid moves starting at such
+    position, then randomly choose one of these moves and returns it. Optionally,
+    can evolve the maze to the next state. If the cursor is stuck (i.e. no move
+    will lead to a white cell), the function returns None.
+
+    Arguments:
+    - ```position: Position```: cursor position
+    - ```evolve: bool```: maze will be evolved to the next state, if enabled
+
+    Returns: randomly determined next position or None, if no move is available
+    """
     if position is None:
       return None
     
@@ -134,7 +237,25 @@ class Maze():
 
     return random.choice(moves)
 
-  def solve(self, starting_position: tuple[int, int] = None, max_steps: int = 100, verbose = False):
+  def solve(self, starting_position: Position = None, max_steps: int = 100, verbose: bool = False) -> list[Position] | None:
+    """
+    Solve the maze by computing all possible paths for a cursor starting at the given
+    position that end up in the finishing cell. For that, a step-by-step evolution
+    of the maze is computed. For every step (corresponding to a given cell state in the
+    maze) every survival path is appended with all possible moves that end up in a white
+    cell. Degenerate, different paths that lead to a same position at a given step are
+    collapsed, since the future steps depend only on the current cell state and cursor
+    position. Once the first path leading to the finishing cell is found, the function
+    returns such path. If after ```max_steps``` no such path is found, the function will
+    return nothing.
+
+    Arguments:
+    - ```position: Position```: starting cursor position
+    - ```max_steps: int```: maximum number of steps
+    - ```verbose: bool```: whether to print step-by-step information or not
+
+    Returns: ```list[Position]```, if a path is found, otherwise returns ```None```
+    """
     if starting_position is None:
       starting_position = self.start
 
@@ -164,7 +285,19 @@ class Maze():
 
     return None
   
-  def solve_random(self, position: tuple[int, int] = None, tries: int = 100):
+  def solve_random(self, position: Position = None, tries: int = 100) -> list[Position]:
+    """
+    Naive approach to solving a maze: make a cursor walk randomly from the starting position,
+    but taking into account only moves that will lead to a white cell. Eventually, if the cursor
+    end up in the finishing cell, a path is returned. If, after ```tries``` attempts, no path
+    leads to the finishing cell, nothing is returned.
+
+    Arguments:
+    - ```position: Position```: starting cursor position
+    - ```tries: int```: maximum number of tries
+
+    Returns: ```list[Position]```, if a path is found, otherwise returns ```None```
+    """
     if position is None:
       position = self.start
 
@@ -185,6 +318,6 @@ class Maze():
         path.append(cur_step)
 
         if cur_step == self.finish:
-          return True, path, itry+1
+          return path
     
-    return False, None, tries
+    return None
