3dd674f461bb74bdd0955aa8ef1866c3422ee4a3
lbarbele <luan.arbeletche@gmail.com>
2023-04-07 03:35:09 -0300

    implement path tree

---
+++ src/path.hpp
@@ -0,0 +1,59 @@
+#ifndef _has_path_hpp_
+#define _has_path_hpp_
+
+#include <list>
+#include <memory>
+
+template <class T>
+class path : public std::enable_shared_from_this<path<T>> {
+public:
+  using position_t = T;
+  using ptr_t = std::shared_ptr<path<position_t>>;
+
+private:
+  position_t _position;
+  ptr_t _previous;
+  std::list<ptr_t> _next;
+
+  path() = default;
+
+  path(const path&) = delete;
+  path& operator=(const path&) = delete;
+
+public:
+
+  static ptr_t create(
+    const position_t position,
+    const ptr_t previous = nullptr
+  )
+  {
+    auto s = ptr_t(new path());
+    s->_position = position;
+    s->_previous = previous;
+    return s;
+  }
+
+  ptr_t get_ptr()
+  {return this->shared_from_this();}
+
+  ptr_t walk(const position_t pos)
+  {
+    auto s = create(pos, get_ptr());
+    _next.push_back(s);
+    return s;
+  }
+
+  const auto get_next() const {return _next;}
+  const auto get_position() const {return _position;}
+  const auto get_previous() const {return _previous;}
+
+  auto get_position_list() {
+    std::list<Position> pos_list;
+    for (auto p = get_ptr(); p != nullptr; p = p->get_previous()) {
+      pos_list.push_front(p->get_position());
+    }
+    return pos_list;
+  }
+};
+
+#endif // _has_path_hpp_
