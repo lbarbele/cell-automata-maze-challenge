c6272027082ac849d48d83142f1cf10395f7b55b
lbarbele <luan.arbeletche@gmail.com>
2023-04-07 00:22:56 -0300

    set/clear cell are public

--- src/maze.hpp
+++ src/maze.hpp
@@ -12,26 +12,25 @@ class Maze {
 private:
   Matrix<cell_t> _config;
 
-  void set_cell(const std::size_t idx);
-  void set_cell(const std::size_t i, const std::size_t j) {set_cell(i*cols + j);}
-  void set_cell(const Position& p) {set_cell(p.x, p.y);}
-  void clear_cell(const std::size_t idx);
-  void clear_cell(const std::size_t i, const std::size_t j) {clear_cell(i*cols + j);}
-  void clear_cell(const Position& p) {clear_cell(p.x, p.y);}
-
   struct Cell {
     static const cell_t dead = 0;
     static const cell_t live = 1;
   };
 
 public:
-
   const Matrix<cell_t>& config = _config;
   const std::size_t& rows = config.rows;
   const std::size_t& cols = config.cols;
 
   Maze(const Maze& other);
   Maze(const Matrix<cell_t>& m, const bool ignore_bad = false);
+
+  void set_cell(const std::size_t idx);
+  void set_cell(const std::size_t i, const std::size_t j) {set_cell(i*cols + j);}
+  void set_cell(const Position& p) {set_cell(p.x, p.y);}
+  void clear_cell(const std::size_t idx);
+  void clear_cell(const std::size_t i, const std::size_t j) {clear_cell(i*cols + j);}
+  void clear_cell(const Position& p) {clear_cell(p.x, p.y);}
 
   static Maze from_file(std::filesystem::path path, const bool ignore_bad = false)
   {return Maze(Matrix<cell_t>::from_file(path), ignore_bad);}
