e9f6a379cc45a239dade1a2027d84cce20202328
lbarbele <luan.arbeletche@gmail.com>
2023-04-07 18:53:05 -0300

    refactoring of path

--- src/path.hpp
+++ src/path.hpp
@@ -1,77 +1,99 @@
 #ifndef _has_path_hpp_
 #define _has_path_hpp_
 
-// ! beware the use of "position" here!
-
 #include <exception>
 #include <list>
 #include <memory>
 
-template <class T>
-class path : public std::enable_shared_from_this<path<T>> {
-public:
-  using position_t = T;
-  using ptr_t = std::shared_ptr<path<position_t>>;
+namespace utl {
 
-private:
-  position_t _position;
-  ptr_t _previous;
-  std::list<ptr_t> _next;
+  template <class T>
+  class path : public std::enable_shared_from_this<path<T>> {
+  public: // member type aliases
+    using pos_t = T;
+    using ptr_t = std::shared_ptr<path>;
 
-  path() = default;
+  private: // data fields
+    pos_t _position;
+    ptr_t _previous;
+    std::list<ptr_t> _next;
 
-  path(const path&) = delete;
-  path& operator=(const path&) = delete;
+  private: // constructors are private: must use factory instead
 
-public:
+    path() = default;
+    path(const path&) = delete;
+    path& operator=(const path&) = delete;
 
-  static ptr_t create(
-    const position_t position,
-    const ptr_t previous = nullptr
-  )
-  {
-    auto s = ptr_t(new path());
-    s->_position = position;
-    s->_previous = previous;
-    return s;
-  }
+  public: // methods
 
-  ptr_t get_ptr()
-  {return this->shared_from_this();}
+    // path factory
 
-  ptr_t walk(const position_t pos)
-  {
-    auto s = create(pos, get_ptr());
-    _next.push_back(s);
-    return s;
-  }
-
-  const auto get_next() const {return _next;}
-  const auto get_position() const {return _position;}
-  const auto get_previous() const {return _previous;}
-
-  auto get_position_list() {
-    std::list<position_t> pos_list;
-    for (auto p = get_ptr(); p != nullptr; p = p->get_previous()) {
-      pos_list.push_front(p->get_position());
-    }
-    return pos_list;
-  }
-
-  void drop() {
-    if (!_next.empty()) {
-      throw std::runtime_error("can not drop a path if _next is not empty");
+    static ptr_t create(
+      const pos_t position
+    )
+    {
+      auto raw_ptr = new path();
+      raw_ptr->_position = position;
+      return ptr_t(raw_ptr);
     }
 
-    if (_previous) {
-      _previous->_next.remove(get_ptr());
-      if (_previous->_next.empty()) {
-        _previous->drop();
+    // helper function to retrieve a shared_ptr of this
+
+    ptr_t get_ptr()
+    {return this->shared_from_this();}
+
+    // walk towards the given position
+
+    ptr_t walk(const pos_t pos)
+    {
+      auto s = create(pos);
+      s->_previous = get_ptr();
+      _next.push_back(s);
+      return s;
+    }
+
+    // data access
+
+    const auto get_next() const {return _next;}
+    const auto get_position() const {return _position;}
+    const auto get_previous() const {return _previous;}
+
+    // convert path into a list of positions
+
+    auto get_position_list() {
+      std::list<pos_t> pos_list;
+      for (auto p = get_ptr(); p != nullptr; p = p->get_previous()) {
+        pos_list.push_front(p->get_position());
       }
+      return pos_list;
     }
 
-    _previous = nullptr;
-  }
-};
+    // drop the current path
+
+    void drop() {
+      if (!_next.empty()) {
+        throw std::runtime_error("paths can only be dropped from their last elements");
+      }
+
+      // if this path is connected to a previous path
+      if (_previous) {
+        // then remove this from the previous path
+        _previous->_next.remove(get_ptr());
+        // if previous is not connected to any other path, drop it too
+        if (_previous->_next.empty()) {
+          _previous->drop();
+        }
+      }
+
+      // remove reference to previous, so it can be deleted
+      _previous = nullptr;
+    }
+
+    ~path() {
+      std::cout << "bye :) " << _position << std::endl;
+    }
+  };
+
+} // utl
 
 #endif // _has_path_hpp_
