09b84491a96f9d12c2e80f0303d102e3d8e339b7
lbarbele <luan.arbeletche@gmail.com>
2023-04-07 03:23:09 -0300

    utility function to get neighbours in a grid

--- src/position.hpp
+++ src/position.hpp
@@ -10,6 +10,19 @@ struct Position {
   const bool operator==(const Position& other) const {
     return x == other.x && y == other.y;
   }
+
+  std::vector<Position> get_neighbours(
+    const std::size_t rows,
+    const std::size_t cols
+  ) const {
+    std::vector<Position> v;
+    v.reserve(4);
+    if (x > 0) v.push_back({x-1, y});
+    if (x < rows-1) v.push_back({x+1, y});
+    if (y > 0) v.push_back({x, y-1});
+    if (x < cols-1) v.push_back({x, y+1});
+    return v;
+  }
 };
 
 // position printer function
