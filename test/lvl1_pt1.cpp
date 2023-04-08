0483c2a7ee2e8790404dae364ba3b51bd1da4049
lbarbele <luan.arbeletche@gmail.com>
2023-04-07 22:25:59 -0300

    implement tests using the first maze from the first level

--- test/lvl1_pt1.cpp
+++ test/lvl1_pt1.cpp
@@ -1,11 +1,61 @@
+#include <exception>
 #include <iostream>
+#include <filesystem>
 
 #include <cell_maze.hpp>
+#include <matrix.hpp>
 #include <position.hpp>
 #include <solve_maze.hpp>
 
+bool
+test_evolution(
+  const utl::cell_maze<uint>& input_maze,
+  const std::filesystem::path data_path,
+  const uint generations
+)
+{
+  auto maze = input_maze;
+
+  for (uint i = 0; i <= generations; ++i) {
+    const auto fname = "lvl1_pt1_" + std::to_string(i) + ".txt";
+    const auto true_states = utl::matrix<uint>::from_file(data_path / fname);
+
+    if (maze.states() != true_states)
+      return false;
+
+    maze.evolve();
+    maze.clear_cell(0);
+    maze.clear_cell(maze.size() - 1);
+  }
+
+  return true;
+}
+
+bool
+test_solution(
+  const std::filesystem::path data_path,
+  const utl::position_list& solution
+)
+{
+
+  uint generation = 0;
+
+  for (const auto& pos : solution) {
+    const auto fname = "lvl1_pt1_" + std::to_string(generation++) + ".txt";
+    const auto states = utl::matrix<uint>::from_file(data_path / fname);
+    if (states[pos]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 int
-main()
+main(
+  int argc,
+  char** argv
+)
 {
   const utl::position_list initial_live_cells = {
     {1, 4},
@@ -27,12 +77,23 @@ main()
   maze.set_overpopulation_rule(7);
   maze.set_reproduction_rules(2, 3);
 
-  const auto first_level_solution = utl::solve_maze(maze, {0, 0}, {6, 7}, 100, 1000, false);
+  // test maze evolution
+  const bool evolution_ok = test_evolution(maze, argv[1], 30);
 
-  if (!first_level_solution.empty()) {
-    std::cout << first_level_solution;
-    return 0;
-  } else {
-    return 1;
+  if (!evolution_ok) {
+    throw std::runtime_error("maze evolution incorrect");
   }
+
+  // test maze solution
+  const auto solution = utl::solve_maze(maze, {0, 0}, {6, 7}, 100, 1000, false);
+
+  const bool solution_ok = test_solution(argv[1], solution);
+
+  if (!solution_ok) {
+    throw std::runtime_error("solution is incorrect");
+  }
+
+  std::cout << "lvl1_pt1: all tests passed :)" << std::endl;
+
+  return 0;
 }
